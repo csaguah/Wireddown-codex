731fef0355d23401059ed0f21e8b0ab3a800310a
csaguah <csaguadc@gmail.com>
2025-06-10 11:10:46 -0400

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,8 @@
+def wire_down_breathing():
+    print("🧘 Inhalar profundo por la nariz... 4 segundos")
+    print("🤲 Mantener... 4 segundos")
+    print("🌬️ Exhalar lentamente por la boca... 6 segundos")
+    print("🌀 Repetir 5 veces para activar el sistema nervioso parasimpático")
+
+if __name__ == "__main__":
+    wire_down_breathing()
